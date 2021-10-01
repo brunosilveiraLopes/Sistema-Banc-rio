e5ad2a22a097e648719667c5e9d1ac7218d29ec0
brunosilveiraLopes <89555108+brunosilveiraLopes@users.noreply.github.com>
2021-10-01 15:10:26 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,29 @@
+package Bancario;
+
+public class Cliente {
+		private String cpf;
+		private String nome;
+		
+		public Cliente(String cpf, String nome) {
+			this.cpf = cpf;
+			this.nome = nome;
+		}
+		
+		public Cliente(String cpf) {
+			this.cpf = cpf;
+		}
+
+		public String getCpf() {
+			return cpf;
+		}
+
+		public String getNome() {
+			return nome;
+		}
+		
+		@Override
+		public String toString() {
+			return String.format("Nome do cliente: %s\nCPF do cliente: %s\n\n", this.nome,this.cpf);
+		}
+
+}
